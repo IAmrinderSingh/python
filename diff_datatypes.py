8c249aa92d34bdaa2d1046afd7a6b5ade77202ed
Amrinder Singh <amrindersingh1703@gmail.com>
2021-09-12 15:45:12 +0530

    different datatype in python

---
+++ diff_datatypes.py
@@ -0,0 +1,25 @@
+# Aim:-Write a program to demonstrate different number data types in python
+# numeric data type can be integer,float,complex in python 
+a=5
+b=2.0
+c=2+3j
+print("Type of a: ",type(a))
+print("Type of b: ", type(b))
+print("Type of c: ", type(c))
+# Sequence data type have string,list,tuple
+string='this line is string'
+print("Type of string: ",type(string))
+list1=[]
+print("Type of list1: ", type(list1))
+tuple1=()
+print("Type of tuple1:" ,type(tuple1))
+# demonstrating Boolean data type
+print("Type of True:",type(True))
+print("Type of False:", type(False))
+# demonstrating set data type
+set1=set()
+print("Type of set1: ", type(set1))
+# demonstrating Dictionary data type
+dictionary={}
+print("Type of dictionary: ", type(dictionary))
+
