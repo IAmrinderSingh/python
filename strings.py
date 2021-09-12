d869da0c4b49eb313ee674d4ae768b930ec21f99
Amrinder Singh <amrindersingh1703@gmail.com>
2021-09-12 15:46:48 +0530

    creation of strings

---
+++ strings.py
@@ -0,0 +1,10 @@
+# Aim:-Write a program to create ,concatenate and print a strings and accessing sub-string from a given string.
+# creating two strings
+first_name="Amrinder"
+last_name="Singh"
+# concatenating two strings together
+fullname=first_name+last_name
+# printing string
+print(fullname)
+# accessing sub-string from a given string.
+print(fullname[:8])
