8361ae1ba7da77714d91a728795b4deeb665ab96
IAmrinderSingh <amrindersingh1703@gmail.com>
2021-10-03 16:43:20 +0530

    input from user

--- binarysearch.py
+++ binarysearch.py
@@ -1,13 +1,21 @@
 def binarysearch(start,end,ele,arr):
-    mid=start+(end-start)//2
-    if arr[mid]==ele:
-        return ele
-    elif arr[mid]>ele:
-        return binarysearch(start,mid-1,ele,arr)
-    elif arr[mid]<ele:
-        return binarysearch(mid+1,end,ele,arr)
+    if end>=start:
+        mid=start+(end-start)//2
+        if arr[mid]==ele:
+            return ele
+        elif arr[mid]>ele:
+            return binarysearch(start,mid-1,ele,arr)
+        else:
+            return binarysearch(mid+1,end,ele,arr)
     else:
         return -1
-arr=[0,1,2,3,4,5,6,7,8,9,10]
-ele=int(input("Enter an number to search in array 0-10: "))
-print(binarysearch(0,len(arr)-1,ele,arr))
+lenarr=int(input("Enter length of array: "))
+arr=[]
+for i in range(lenarr):
+    arr.append(int(input(f"Enter [{i}] element: ")))
+ele=int(input("Enter an number to search in array : "))
+result=binarysearch(0,len(arr)-1,ele,arr)
+if (result==-1):
+    print("Number not exist")
+else:
+    print("Element is :",result)
